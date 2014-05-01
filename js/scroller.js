dba54335bc1020496084ef8404c24b491f107c7c
Tamas Erdos <tamas@tamaserdos.com>
2014-05-01 10:40:11 -0400

    adding timer

--- js/scroller.js
+++ js/scroller.js
@@ -1,4 +1,10 @@
     'use strict';
+    var SYK = {}
+    SYK.rotate = function(banner, newframe) {
+        banner.removeClass('frame1 frame2 frame3');
+        banner.addClass(newframe);
+    }
+
     $(function() {
         $('.adScroller').each(function() {
             var div = $(this);
@@ -14,6 +20,22 @@
         $('img.restart').each(function() {
             var img = $(this);
             var src = img.attr('src');
-            img.attr('src', src+'?'+Date.now());
+            img.attr('src', src + '?' + Date.now());
         });
+        for (var i = 1; i <= 3; i++) {
+            $('.sykbanner_image').each(function() {
+                var banner = this
+                window.setTimeout(function() {
+                    SYK.rotate($(banner), 'frame2');
+                }, 3000 + 15000 * (i - 1));
+                window.setTimeout(function() {
+                    SYK.rotate($(banner), 'frame3');
+                }, 10000 + 15000 * (i - 1));
+                if (i < 3) {
+                    window.setTimeout(function() {
+                        SYK.rotate($(banner), 'frame1');
+                    }, 15000 + 15000 * (i - 1));
+                }
+            });
+        }
     });
